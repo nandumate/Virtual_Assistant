19e74955d027223f51596578d8a14e7a232427e6
Nandini Mate <142236551+nandumate@users.noreply.github.com>
2025-01-25 15:56:11 +0530

    Add files via upload

---
+++ Virtual_Assistant/script.js
@@ -0,0 +1,86 @@
+let btn = document.querySelector("#btn");
+let content = document.querySelector("#content");
+let voice = document.querySelector("#voice");
+
+function speak(text) {
+    let text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+    text_speak.volume = 1;
+    text_speak.lang = "en-GB";
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    let day = new Date();
+    let hours = day.getHours();
+
+    if (hours >= 0 && hours < 12) {
+        speak("Good Morning Sir");
+    } else if (hours >= 12 && hours < 16) {
+        speak("Good Afternoon Sir");
+    } else {
+        speak("Good Evening Sir");
+    }
+}
+
+let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (SpeechRecognition) {
+    let recognition = new SpeechRecognition();
+
+    recognition.onresult = (event) => {
+        let currentIndex = event.resultIndex;
+        let transcript = event.results[currentIndex][0].transcript;
+        content.innerText = transcript;
+        takeCommand(transcript.toLowerCase());
+    };
+
+    btn.addEventListener("click", () => {
+        recognition.start();
+        btn.style.display = "none";
+        voice.style.display = "block";
+    });
+} else {
+    console.error("SpeechRecognition API is not supported in this browser.");
+    btn.addEventListener("click", () => {
+        alert("SpeechRecognition API is not supported in this browser.");
+    });
+}
+
+function takeCommand(message) {
+    btn.style.display = "flex";
+    voice.style.display = "none";
+
+    if (message.includes("hello") || message.includes("hey")) {
+        speak("Hello sir, what can I help you with?");
+    } else if (message.includes("who are you")) {
+        speak("I am a virtual assistant, created by Nandini");
+    } else if (message.includes("open youtube")) {
+        speak("Opening YouTube...");
+        window.open("https://youtube.com/", "_blank");
+    } else if (message.includes("open google")) {
+        speak("Opening Google...");
+        window.open("https://google.com/", "_blank");
+    } else if (message.includes("open calculator")) {
+        speak("Opening calculator...");
+        window.open("calculator://");
+    } else if (message.includes("open whatsapp")) {
+        speak("Opening WhatsApp...");
+        window.open("whatsapp://");
+    } else if (message.includes("time")) {
+        let time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
+        speak(time);
+    } else if (message.includes("date")) {
+        let date = new Date().toLocaleString(undefined, { day: "numeric", month: "short" });
+        speak(date);
+    } else {
+        let finalText = "This is what I found on the internet regarding " + message.replace("shifra", "");
+        speak(finalText);
+        window.open(`https://www.google.com/search?q=${message.replace("shifra", "")}`, "_blank");
+    }
+}
+
+/*window.addEventListener('load', () => {
+    wishMe();
+});*/
